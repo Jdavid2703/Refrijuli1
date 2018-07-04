60d3ef8487305e3fd1e1bf0c1c279191d4b82194
indep <independientemedellin.27.03@gmail.com>
2018-07-04 10:44:27 -0500

    actualización vista usuario JD..

--- resources/views/admin/usuarios.blade.php
+++ resources/views/admin/usuarios.blade.php
@@ -15,57 +15,20 @@ if (!$enlace) {
 
 <div class="container-fluid ">
 
-    <div class="row ">
-        <div class="col-md-2  ">
-            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-            <table class="table">
+    <div class="row">
+        <div class="col-md-2">
 
-                <thead class="thead-dark">
-                <tr align="center">
-                  <h3 align="center"><b>ROLES</b></h3>
-                </tr>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">DESCRIPCIÓN</th>
-
-                </tr>
-                </thead>
-
-
-                <tbody>
-
-            <?php
-
-            $sql = "SELECT id,descripcion FROM rols ";
-            $result = mysqli_query($enlace, $sql);
-
-
-            while ($mostrar = mysqli_fetch_array($result)){
-            ?>
-            <tr>
-
-                <td><?php echo $mostrar['id']?></td>
-                <td><?php echo $mostrar['descripcion']?></td>
-
-
-
-            </tr>
-            <?php
-            }
-            ?>
-                </tbody>
-            </table>
         </div>
 
 
         <div class="col-md-8 ">
             <div align="center">
-                <h1><b>USUARIOS</b> </h1>
+                <h1><b>USUARIOS</b></h1>
 
 
-                    <form action="{{route('registro')}}">
-                        <input class="btn btn-primary" type="submit" value="Nuevo">
-                    </form>
+                <form action="{{route('registro')}}">
+                    <input class="btn btn-primary" type="submit" value="Nuevo">
+                </form>
 
 
             </div>
@@ -101,7 +64,7 @@ if (!$enlace) {
                 <tr>
 
                     <td><?php echo $mostrar['id']?></td>
-                    <td ><?php echo $mostrar['nombre']?></td>
+                    <td><?php echo $mostrar['nombre']?></td>
                     <td><?php echo $mostrar['apellido']?></td>
                     <td><?php echo $mostrar['numero']?></td>
                     <td><?php echo $mostrar['correo']?></td>
@@ -113,13 +76,10 @@ if (!$enlace) {
                         <a class="btn btn-primary" href="#">Editar</a>
 
 
-
                 </tr>
                 <?php
                 }
                 ?>
-
-
 
 
                 </tbody>
@@ -133,9 +93,9 @@ if (!$enlace) {
 
 
         </div>
-
     </div>
 </div>
+
 
 
 
