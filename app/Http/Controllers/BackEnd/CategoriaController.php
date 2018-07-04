864644a124bdbbec91597b81bc0d786d6055db52
Astrid Perez <mmalzate62@misena.edu.co>
2018-07-04 17:47:28 -0500

    actualización categoria controller Mateo

---
+++ app/Http/Controllers/BackEnd/CategoriaController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\BackEnd;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoriaController extends Controller
+{
+
+    public function index()
+    {
+        $categoria = \App\Models\Categoria::all();
+        return view('admin.categoria',compact('categoria'));
+    }
+
+
+    public function create()
+    {
+
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.#categori{color: white;}#categori:hover{color: orange}
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
