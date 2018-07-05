ee2f0cdb8feaa5538fc9d622bc9cf3f3da6698f6
indep <independientemedellin.27.03@gmail.com>
2018-07-05 07:29:38 -0500

    actualización diseño JD..

--- resources/views/componentes/contenido.blade.php
+++ resources/views/componentes/contenido.blade.php
@@ -18,17 +18,14 @@
     <div class="row">
 
         <div class="col-md-6">
-            <div class="card mb-3">
-                <img class="card-img-top img-fluid w-100"
-                     src="http://1.bp.blogspot.com/-PYHRf4t13DM/V3320p4ncjI/AAAAAAAAB6U/rod8bPLJuecJOwjA4QWmuq2xxEJGUoL5gCK4B/s1600/burger.jpg">
 
-                <div class="card-body">
-                    <h6 class="card-title mb-1"><b>La super Hamburguesa Artesanal</b></h6>
-                    <p class="card-text small">Pruebala!
-                        <a href="#">#refrujuli</a>
-                    </p>
-                </div>
-            </div>
+            <h4>
+                <ins><b><a id="seguiPedidos" class="nav-link {{request()->is('contactos')?'active':''}}"
+                           href="{{route('contactos')}}">Click aquí para ver seguimiento de pedidos</a></b></ins>
+            </h4>
+            <p>Los pedidos se hacen mediante llamada telefonica, se le otorga un <b>ID de Usuario</b> y puede entrar a la
+                página constantemente
+                para ver el estado del pedido.</p>
 
             <div class="col-md-1">
             </div>
@@ -37,13 +34,13 @@
 
             <div class="card mb-3">
                 <img class="card-img-top img-fluid w-100"
-                     src="http://www.saladworks.com/sites/default/files/Sandwich-AvocadoBLT.jpg">
+                     src="http://1.bp.blogspot.com/-PYHRf4t13DM/V3320p4ncjI/AAAAAAAAB6U/rod8bPLJuecJOwjA4QWmuq2xxEJGUoL5gCK4B/s1600/burger.jpg">
 
                 <div class="card-body">
-                    <h6 class="card-title mb-1"><b>Sandwiches de verdura</b></h6>
-                    <p class="card-text small">No te quedes con las ganas!
+
+                    <h6 class="card-title mb-1"><b>La super Hamburguesa Artesanal</b></h6>
+                    <p class="card-text small">Pruebala!
                         <a href="#">#refrujuli</a>
-                        <a href="#">#vegetal</a>
                     </p>
                 </div>
             </div>
@@ -52,32 +49,47 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-6">
+    {{--<div class="row">--}}
+        {{--<div class="col-md-1">--}}
+        {{--</div>--}}
+        {{--<div class="col-md-5">--}}
 
-            <div class="card mb-3">
-                <a href="#">
-                    <img class="card-img-top img-fluid w-100"
-                         src="https://www.vvsupremo.com/wp-content/uploads/2016/02/900X570_Mexican-Style-Hot-Dogs.jpg"
-                         alt="">
-                </a>
-                <div class="card-body">
-                    <h6 class="card-title mb-1"><a href="#">Perros calienticos!</a></h6>
-                    <p class="card-text small">De lo mejor que hay, con un condimento especial
-                        <a href="#">#refrijuli</a>
-                        <a href="#">#hotdog</a>
-                        <a href="#">#parallevar</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-6">
-
-            <h4><ins><b><a id="seguiPedidos" class="nav-link {{request()->is('contactos')?'active':''}}"
-                      href="{{route('contactos')}}">Seguimiento de pedidos</a></b></ins></h4>
-        </div>
+            {{--<div class="card mb-3">--}}
+                {{--<a href="#">--}}
+                    {{--<img class="card-img-top img-fluid w-100"--}}
+                         {{--src="https://www.vvsupremo.com/wp-content/uploads/2016/02/900X570_Mexican-Style-Hot-Dogs.jpg"--}}
+                         {{--alt="">--}}
+                {{--</a>--}}
+                {{--<div class="card-body">--}}
+                    {{--<h6 class="card-title mb-1"><a href="#">Perros calienticos!</a></h6>--}}
+                    {{--<p class="card-text small">De lo mejor que hay, con un condimento especial--}}
+                        {{--<a href="#">#refrijuli</a>--}}
+                        {{--<a href="#">#hotdog</a>--}}
+                        {{--<a href="#">#parallevar</a>--}}
+                    {{--</p>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
 
-    </div>
+        {{--<div class="col-md-5">--}}
+
+            {{--<div class="card mb-3">--}}
+                {{--<img class="card-img-top img-fluid w-100"--}}
+                     {{--src="http://www.saladworks.com/sites/default/files/Sandwich-AvocadoBLT.jpg">--}}
+
+                {{--<div class="card-body">--}}
+                    {{--<h6 class="card-title mb-1"><b>Sandwiches de verdura</b></h6>--}}
+                    {{--<p class="card-text small">No te quedes con las ganas!--}}
+                        {{--<a href="#">#refrujuli</a>--}}
+                        {{--<a href="#">#vegetal</a>--}}
+                    {{--</p>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+
+        {{--</div>--}}
+
+
+    {{--</div>--}}
 </div>
